bd84d50d06ad435356cbd9b65512a332f4b0161b
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-19 16:59:06 +0530

    [Rinkesh] : add Added Validate Email address with a

---
+++ EmailValidation.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EmailValidation {
+
+	public static final Scanner scan = new Scanner(System.in);
+
+	public static void checkEmail() {
+		System.out.println("......Email Validation......");
+		System.out.println("Enter Your Email Id");
+		String emailId = scan.nextLine();
+		String regex = "^[a-zA-Z0-9]*";
+			if (emailId.matches(regex)) {
+				System.out.println("This Email Id Is Valid");
+
+			} else {
+				System.out.println("This Email Id is Invaild");
+
+			}
+
+	}
+
+	public static void main(String[] args) {
+		//calling method to check Email Validation
+		checkEmail();
+
+	}
+
+}
+
